f21ef1329f8e248ce749e9cbd8ccd04533733941
Kilian Lieret <kilian.lieret@posteo.de>
2025-07-21 21:28:14 -0400

    Fix(default): Show action in timeout message (#236)
    Closes #232

--- src/microsweagent/agents/default.py
+++ src/microsweagent/agents/default.py
@@ -18,9 +18,8 @@ class AgentConfig:
         "To finish, the first line of the output of the shell command must be 'MICRO_SWE_AGENT_FINAL_OUTPUT'."
     )
     timeout_template: str = (
-        "The last command {{action}} timed out and has been killed.\n"
-        "The output of the command was:\n"
-        "{{output}}\n\n"
+        "The last command <command>{{action['action']}}</command> timed out and has been killed.\n"
+        "The output of the command was:\n <output>\n{{output}}\n</output>\n"
         "Please try another command and make sure to avoid those requiring interactive input."
     )
     format_error_template: str = "Please always provide EXACTLY ONE action in triple backticks."
